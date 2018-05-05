712993635abbe598cbea7082b709264f40fbf3f8
tangyj <2495944310@qq.com>
2018-05-05 13:30:37 +0800

    完成修改warning.js by剑

--- static/js/warning.js
+++ static/js/warning.js
@@ -1,5 +1,4 @@
-var a = document.getElementsByName("box1"); 
-console.log(typeof a);
+
 function validate_required(field,alerttxt)
 {
 with (field)
@@ -11,26 +10,27 @@ with (field)
 }
 function validate_required1(field,b,alerttxt)
 {
-with (field)
+with (field,b)
   {
-  if ((b.value==0))
-    {alert("alerttxt");
+  if ((b==false))
+    {
       return true}
   else {
-  
+  if( field.value==""){
     alert(alerttxt);
-    return false}
+    return false}}
   }
 }
 
 function validate_form(thisform)
-{
+{var a = document.getElementsByName("box1"); 
 with (thisform)
   {
   if (validate_required(xia,"请选择相册!")==false)
     {xia.focus();return false}
-  //if (validate_required1(fk0,box1,"请选择照片或者改为不上传")==false)
-    //{fk0.focus();return false}
+  if (validate_required1(fk0,a[1].checked,"请选择照片或者改为不上传")==false)
+    {fk0.focus();return false}
     
   }
+
 }
