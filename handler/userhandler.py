cfdb11f7bd35c9541911177c292f20b5e7329e6c
fengyulin <494800279@qq.com>
2018-05-06 12:35:26 +0800

    密码存储方式更改

--- handler/userhandler.py
+++ handler/userhandler.py
@@ -33,14 +33,28 @@ class AuthCreateHandler(BaseHandler):
         #hashed_password = yield executor.submit(
         #    bcrypt.hashpw, tornado.escape.utf8(self.get_argument("password")),
         #    bcrypt.gensalt())
-        author_id = self.db.execute(
-            "INSERT INTO users (email, name, hashed_password,nickname,user_description) "
-            "VALUES (%s, %s, %s,%s,%s)",
-            self.get_argument("email"), self.get_argument("name"),
-            self.get_argument("password"), self.get_argument("nickname"),
-            self.get_argument("user_description"))
-        self.set_secure_cookie("cur_user", str(author_id))
-        self.redirect(self.get_argument("next", "/"))
+        email=self.get_argument("email")
+        name=self.get_argument("name")
+        password=self.get_argument("password")
+        nickname=self.get_argument("nickname")
+        user_description=self.get_argument("user_description")
+        password_again=self.get_argument("password_again")
+        #将password以utf-8解码之后才能哈希
+        hashed_password = bcrypt.hashpw(password.encode(encoding='UTF-8'), bcrypt.gensalt())
+
+        if(hashed_password!=password_again):
+            self.render("create_author.html")    #密码确认
+        if(email and name and password):
+            author_id = self.db.execute(    #存加密过的密码
+                "INSERT INTO users (email, name, hashed_password,nickname,user_description) "
+                "VALUES (%s, %s, %s,%s,%s)",
+                self.get_argument("email"), self.get_argument("name"),
+                hashed_password, self.get_argument("nickname"),
+                self.get_argument("user_description"))
+            self.set_secure_cookie("cur_user", str(author_id))
+            self.redirect(self.get_argument("next", "/login"))
+        else:
+            self.render("create_author.html")
 
 
 class AuthLoginHandler(BaseHandler):
@@ -62,11 +76,16 @@ class AuthLoginHandler(BaseHandler):
         if not author:
             self.render("login.html", error="email not found")
             return
-        hashed_password =self.get_argument("password")
+        password=self.get_argument("password")
+
+
+        #将登录界面获取的password加密
+        hashed_password = bcrypt.hashpw(password.encode(encoding='UTF-8'),author.hashed_password.encode(encoding='UTF-8'))
+
         #hashed_password = yield executor.submit(
         #   bcrypt.hashpw, tornado.escape.utf8(self.get_argument("password")),
         #    tornado.escape.utf8(author.hashed_password))
-        if hashed_password == author.hashed_password:
+        if hashed_password == author.hashed_password.encode(encoding='UTF-8'):
             self.set_secure_cookie("cur_user", str(author.id))    #demo里面是blogdemo_user，这里要改
             self.redirect(self.get_argument("next", "/albums"))
         else:
